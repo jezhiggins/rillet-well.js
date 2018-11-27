7d7660196e9d604ed8a3659cd24bb4f7b86c727b
Jez Higgins <jez@jezuk.co.uk>
2018-11-27 12:01:29 +0000

    work on capturing events into the async pipeline

--- rillet-well.js
+++ rillet-well.js
@@ -1,15 +1,70 @@
+function oneEvent(evtSource, evtName) {
+  let callback = null
+  const p = new Promise(resolve => callback = resolve)
+  evtSource.on(evtName, callback)
+
+  let done = false
+
+  const unattach = d => !d && evtSource.off(evtName, callback)
+
+  return {
+    async next() {
+      if (done)
+        return { done: true }
+
+      const result = { value: await p, done: false };
+      unattach(done);
+      done = true;
+      return result;
+    },
+    return() {
+      unattach(done);
+    },
+    [Symbol.asyncIterator]() {
+      return this;
+    }
+  }
+} // oneEvent
+
+async function* eventStream(evtSource, evtName) {
+  let callback = null
+  let p = new Promise(resolve => callback = resolve)
+  const eventSink = v => callback(v)
+
+  try {
+    evtSource.on(evtName, eventSink)
+
+    while (true) {
+      yield p
+
+      p = new Promise(resolve => callback = resolve)
+    } // while
+  } finally {
+    callback = null
+    evtSource.off(evtName, callback)
+  }
+} // eventer
+
 ////////////////////
 class RilletWell {
   static source(async_iter) {
-    return new RilletWell(async_iter)
+    return new RilletWell(async_iter);
   } // source
+
+  static fromEvent(evtSource, evtName) {
+    return new RilletWell(oneEvent(evtSource, evtName));
+  } // fromEvent
+
+  static fromEvents(evtSource, evtName) {
+    return new RilletWell(eventStream(evtSource, evtName));
+  } // fromEvent
 
   constructor(async_iter) {
     this.async_iter = async_iter
   } // constructor
 
-  async* [Symbol.asyncIterator]() {
-    yield* this.async_iter;
+  [Symbol.asyncIterator]() {
+    return this.async_iter;
   }
 
   async toArray(dest = []) {
---
+++ test/test-fromEvent.js
@@ -0,0 +1,85 @@
+const { fromEvent, fromEvents } = require('../rillet-well')
+const { expect } = require('chai')
+const { pause } = require('./helpers');
+
+describe('fromEvent', () => {
+  class EventSource {
+    constructor() {
+      this.eventName = null;
+      this.eventSink = null;
+    }
+
+    on(name, callback) {
+      this.eventName = name;
+      this.eventSink = callback;
+    }
+
+    off(name, callback) {
+      expect(name).to.equal(this.eventName)
+      expect(callback).to.equal(this.eventSink)
+    }
+
+    evt(value) {
+      if (this.eventSink)
+        this.eventSink(value);
+    }
+  } // EventSource
+
+  it ('fromEvent(s, n) toArray', async () => {
+    const eventer = new EventSource()
+    const gather = []
+
+    fromEvent(eventer, 'ping').toArray(gather)
+
+    await pause()
+    eventer.evt('woo')
+    await pause()
+
+    expect(gather).to.eql(['woo'])
+  })
+
+  it ('fromEvent(s, n)', async () => {
+    const eventer = new EventSource()
+
+    const reactor = fromEvent(eventer, 'ping')
+
+    eventer.evt('hooray')
+
+    const result = []
+    for await (const v of reactor)
+      result.push(v)
+
+    expect(result).to.eql(['hooray'])
+  })
+
+
+  it ('fromEvents(s, n) toArray', async () => {
+    const eventer = new EventSource()
+    const gather = []
+
+    fromEvents(eventer, 'ping').toArray(gather)
+
+    for (let i = 1; i != 5; ++i) {
+      await pause()
+      eventer.evt(i*i)
+    }
+    await pause()
+
+    expect(gather).to.eql([1, 4, 9, 16])
+  })
+
+  xit ('fromEvents(s, n)', async () => {
+    const eventer = new EventSource()
+
+    const reactor = fromEvents(eventer, 'ping')
+
+    for (let i = 1; i != 5; ++i)
+      eventer.evt(i*i)
+
+    const result = []
+    for await (const v of reactor)
+      result.push(v)
+
+    expect(result).to.eql([1, 4, 9, 16])
+  })
+})
