cff9dba9f68251eb489493ba96b1f223982b06fd
Jez Higgins <jez@jezuk.co.uk>
2018-11-28 13:46:30 +0000

    slow array test

--- test/test-toArray.js
+++ test/test-toArray.js
@@ -9,6 +9,13 @@ async function* array_gn() {
     yield Promise.resolve(i)
 }
 
+async function* slow_array_gn() {
+  for (const i of array) {
+    yield Promise.resolve(i)
+    await pause(100)
+  }
+}
+
 describe('toArray', () => {
   it('await source(array) toArray', async () => {
     const c = await source(array_gn()).toArray()
@@ -30,6 +37,15 @@ describe('toArray', () => {
     await pause()
 
     expect(gather).to.eql(array)
+  })
+
+  it('source(slow_array) toArray(dest)', async () => {
+    const gather = []
+    source(slow_array_gn()).toArray(gather)
+    await pause(400)
+    expect(gather).to.eql([1,2,3,4])
+    await pause(400)
+    expect(gather).to.eql([1,2,3,4,5,6,7,8])
   })
 
   //t('take(4).count', source(array).take(4).count(), 4);
